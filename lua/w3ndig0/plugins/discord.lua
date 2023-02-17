ac26e66a168cb4f0a6877090b57b0a9cff750590
W3ndig0 <Jingxiang304@gmail.com>
2023-02-17 01:39:59 +0100

    somethign uesful💀

--- lua/w3ndig0/plugins/discord.lua
+++ lua/w3ndig0/plugins/discord.lua
@@ -14,10 +14,10 @@ require("presence").setup({
     show_time           = true,                       -- Show the timer
 
     -- Rich Presence text options
-    editing_text        = "Imagine acutally coding %s 💀🤡",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-    file_explorer_text  = "Pretending to be looking at my code, bcs im lost %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    editing_text        = "Imagine coding %s 💀🤡",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "imagine enjoying debugging time💀 (%s)",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
     git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)init
     reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
     workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
     line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
